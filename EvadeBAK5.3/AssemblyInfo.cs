f94f941b9726542b9e1a78508d0ba6efc8384f6b
xGoldenS <thaison.1294@gmail.com>
2015-03-02 16:31:49 +0700

    Update AssemblyInfo.cs

--- EvadeBAK5.3/AssemblyInfo.cs
+++ EvadeBAK5.3/AssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Evade")]
+[assembly: AssemblyTitle("Evade for 5.3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
